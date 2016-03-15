159612c0bcf62290bf38b051c8012b104f3b1301
Shrikanta Sharma <shrikss@gmail.com>
2016-03-15 16:24:55 +0530

    For initial testing.

--- library/test.sh
+++ library/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ../ansible/hacking/test-module -m sdapi.py -a "junosspace_host=junosspace1.kdc.jnpr.net device=labsrx source_zone=lab junosspace_password=juniper123 change_request_id=cr100 services=http destination_zone=jnpr junosspace_username=super source_addresses=100.100.100.100 destination_addresses=200.200.200.200"
+
+echo "done"
